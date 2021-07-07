2ab6d6850415b646e2b26b9ad80abce7731e0f8d
kelliewatson <kelliewatson@gmail.com>
2021-07-07 15:40:53 -0700

    Website setup

---
+++ src/WatsonXYZ.js
@@ -0,0 +1,25 @@
+import './WatsonXYZ.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function WatsonXYZ() {
+  return (
+    <div className="Watson-xyz">
+    <header>
+      <h1 className="Company-logo">WatsonXYZ </h1>
+      <div>
+        <h2>Subject: </h2>
+        <p> Personal & Small Business Finances made practical.
+        </p>
+        <h2>Company Purpose:</h2>
+        <p> To help each person of our diverse world to embrace and highlight life’s opportunities while avoiding unnecessary financial hazards, thus transforming lives and expanding human potential.
+        </p>
+        <h2>Mission Statement:</h2>
+          <p> To leverage advancing technologies to provide financial and organizational guidance to both individuals and small businesses.
+    </p>
+    </div>
+    </header>
+    </div>
+  );
+}
+
+export default WatsonXYZ;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import WatsonXYZ from './WatsonXYZ';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WatsonXYZ />
   </React.StrictMode>,
   document.getElementById('root')
 );
