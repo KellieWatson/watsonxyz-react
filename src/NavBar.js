57f02eec15aaf60a9405f8bc1972a9b4a41870a4
kelliewatson <kelliewatson@gmail.com>
2021-07-13 21:51:34 -0700

    added NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import {Navbar,Nav} from "react-bootstrap";
 import { Link } from 'react-router-dom';
+
+import './style/NavBar.css'
 
 const NavBar=()=>{
     return(
-        <nav >
-            <ul>
-                <li><Link to="/" className="Topics">WatsonXYZ</Link></li>
-                <li><Link to="/Finance" className="Topics">Finance</Link></li>
-                <li><Link to="/Technology" className="Topics">Technology</Link></li>
-                <li><Link to="/Tools" className="Topics">Tools</Link></li>
-                <li><Link to="/AboutUs" className="Topics">About</Link></li>
-            </ul>
-        </nav>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#Home">WatsonXYZ</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="mr-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                <Link to="/" className="Topics">Home</Link>
+                <Link to="/Finance" className="Topics">Finance </Link>
+               <Link to="/Technology" className="Topics">Technology </Link>
+               <Link to="/Tools" className="Topics">Tools </Link>
+                <Link to="/AboutUs" className="Topics">About</Link>
+
+
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
